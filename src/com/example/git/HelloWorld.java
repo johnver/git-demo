aa5d16cc376a3c44d727649cf72dc1ba8fafe0cd
johnver <johnver@gmail.com>
2014-08-15 16:22:17 +1000

    Hello World 2

--- src/com/example/git/HelloWorld.java
+++ src/com/example/git/HelloWorld.java
@@ -13,7 +13,9 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
 		System.out.println("Hello World!");
+		System.out.println("Hello World 2x");
 
 	}
 

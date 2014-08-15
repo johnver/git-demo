0aa685ab1629c76939ad6108cf3766811231d600
johnver <johnver@gmail.com>
2014-08-15 16:13:31 +1000

    Hello World

---
+++ src/com/example/git/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.example.git;
+
+/**
+ * @author johnver
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+	}
+
+}
